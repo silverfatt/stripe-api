50198f4c65cbfe8dd5fc9eec2a2d3176353a2ab5
Silvestr <90452368+silverfatt@users.noreply.github.com>
2022-09-26 15:03:06 +0300

    Update settings.py

--- stripeapi/settings.py
+++ stripeapi/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "stripeapi.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-dev db
+#dev db
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
