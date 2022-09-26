8ff627448c16952f274a01f89a3bda461afbd141
Silvestr <90452368+silverfatt@users.noreply.github.com>
2022-09-26 15:03:23 +0300

    Update settings.py

--- stripeapi/settings.py
+++ stripeapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a1b=l)%n_++&a-g79(km@!8%vdu2x)4yr^+sb0@41&ayx5gv80"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
